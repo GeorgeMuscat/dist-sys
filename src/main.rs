dba3ba44d7f43b9422a471e85e97aba8522507e8
George Muscat <georgemuscat@proton.me>
2024-04-30 03:43:19 +1000

    passing echo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,114 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{StdoutLock, Write};
+
+use anyhow::{bail, Context};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct Message {
+    src: String,
+    #[serde(rename = "dest")]
+    dst: String,
+    body: Body,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct Body {
+    #[serde(rename = "msg_id")]
+    id: Option<usize>,
+    in_reply_to: Option<usize>,
+    #[serde(flatten)]
+    payload: Payload,
+}
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(tag = "type")]
+#[serde(rename_all = "snake_case")]
+enum Payload {
+    // Received Echo command
+    Echo {
+        echo: String,
+    },
+
+    // Reply to an Echo command
+    EchoOk {
+        echo: String,
+    },
+    Error {
+        text: String,
+    },
+    Init {
+        node_id: String,
+        node_ids: Vec<String>,
+    },
+    InitOk,
+}
+
+struct EchoNode {
+    id: usize,
+}
+
+impl EchoNode {
+    pub fn new() -> Self {
+        Self { id: 0 }
+    }
+
+    pub fn step(&mut self, input: Message, output: &mut StdoutLock) -> anyhow::Result<()> {
+        match input.body.payload {
+            // We received an Echo command
+            Payload::Echo { echo } => {
+                let reply = Message {
+                    src: input.dst,
+                    dst: input.src,
+                    body: Body {
+                        id: Some(self.id),
+                        in_reply_to: input.body.id,
+                        payload: Payload::EchoOk { echo },
+                    },
+                };
+
+                // &mut * is called a "reborrow"
+                // This allows to_writer to take ownership of the mutable borrow that is output.
+                // A reborrow is essentially us dereferencing the mutable reference to the StdoutLock
+                // and initiating a new mutable reference. Why is this allowed? Why does this allow us to use the mutable reference after the function call?
+                //  - I think possibly because output still is never moved.
+                serde_json::to_writer(&mut *output, &reply)?;
+                output.write_all(b"\n")?;
+            }
+            Payload::EchoOk { echo } => todo!(),
+            Payload::Error { text } => todo!(),
+            Payload::Init { .. } => {
+                // When Init is received, we want to reply saying that we have initialised the node
+                let reply = Message {
+                    src: input.dst,
+                    dst: input.src,
+                    body: Body {
+                        id: Some(self.id),
+                        in_reply_to: input.body.id,
+                        payload: Payload::InitOk,
+                    },
+                };
+                serde_json::to_writer(&mut *output, &reply)?;
+                output.write_all(b"\n")?;
+            }
+            Payload::InitOk => bail!("Received InitOk Message"),
+        };
+
+        // Increment id every time or only when we
+        Ok(())
+    }
+}
+
+fn main() -> anyhow::Result<()> {
+    let stdin = std::io::stdin().lock();
+    let inputs = serde_json::Deserializer::from_reader(stdin).into_iter::<Message>();
+
+    let mut stdout = std::io::stdout().lock();
+
+    let mut node = EchoNode::new();
+
+    for input in inputs {
+        let input = input.context("Maelstrom input from STDIN could not be deserialized")?;
+        node.step(input, &mut stdout)?;
+    }
+
+    Ok(())
 }
